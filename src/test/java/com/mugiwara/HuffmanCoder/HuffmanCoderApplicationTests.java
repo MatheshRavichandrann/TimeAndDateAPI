74c6e40a6c6f7668fce442b65fa24b794128c6e0
Mathesh <mathesh1907@gmail.com>
2024-12-05 12:22:45 +0530

    Muhahaha

---
+++ src/test/java/com/mugiwara/HuffmanCoder/HuffmanCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugiwara.HuffmanCoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
