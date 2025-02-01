5dade5537cddb93312b92516a3639f910b2f8345
Mathesh <mathesh1907@gmail.com>
2025-02-01 14:25:38 +0530

    yo yo

---
+++ src/main/java/com/mugiwara/HuffmanCoder/webconfig/WebConfig.java
@@ -0,0 +1,13 @@
+package com.mugiwara.HuffmanCoder.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:5173"); // or wherever your frontend runs
+    }
+}
